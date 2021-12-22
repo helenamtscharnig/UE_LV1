910279f1ae6a393c3cbdebc711b3469aa8557918
helenamtscharnig <s50824@edu.campus02.at>
2021-12-22 19:33:32 +0100

    Commit class VariableDemo

---
+++ src/VariableDemo.java
@@ -0,0 +1,9 @@
+public class VariableDemo {
+    public static void main(String[] args) {
+        String firstname = "Helena";
+        String lastname = "Tscharnig";
+
+        System.out.println("Der Name lautet: "+firstname+" "+lastname);
+    }
+
+}
