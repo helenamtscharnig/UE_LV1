8ed1c244d41d7a5d0b157081d9a2ad6299ebb796
helenamtscharnig <s50824@edu.campus02.at>
2021-12-22 19:36:57 +0100

    Commit class RechteckDemo

---
+++ src/RechteckDemo.java
@@ -0,0 +1,14 @@
+public class RechteckDemo {
+    public static void main(String[] args) {
+        int breite = 2;
+        int laenge = 3;
+
+        int umfang = breite*2+laenge*2;
+        int flaeche = breite*laenge;
+
+        System.out.println("Die Fläche des Rechtecks ist: "+flaeche);
+        System.out.println("Der Umfang des Rechtecks ist: "+umfang);
+
+    }
+
+}
