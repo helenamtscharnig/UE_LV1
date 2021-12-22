8bffb64415a55fbd4fc13e70eb3b4e97cc1d1ff8
helenamtscharnig <s50824@edu.campus02.at>
2021-12-22 20:31:18 +0100

    Commit Erweiterte Zinsberechnung

--- src/ZinsDemo.java
+++ src/ZinsDemo.java
@@ -4,11 +4,21 @@ public class ZinsDemo {
         double kontostand = 1000;
         double zinsen = 2.25;
 
+        double kontostand1 = 2000;
+        double zinsen2 = 5.2;
+
+
+        System.out.println("Der Kontostand ist: "+Kontostand(kontostand, zinsen));
+        System.out.println("Der Kontostand ist: "+Kontostand(kontostand1, zinsen2));
+
+    }
+
+    public static double Kontostand(double kontostand, double zinsen){
+
         double intermediate = kontostand * zinsen / 100;
         double newKontostand = kontostand + intermediate;
 
-        System.out.println("Der Kontostand ist: "+newKontostand);
-
+        return newKontostand;
     }
 
 
